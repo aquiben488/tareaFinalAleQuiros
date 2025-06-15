a73fe26e355b3564f983dcaf8f6c6d589ea1553c
Alejandro Quirós <aquiben488@g.educaand.es>
2025-06-15 21:42:03 +0200

    actualizado el main frame para modularizar la UI

--- ProtectoJPA/src/main/java/views/MainFrame.java
+++ ProtectoJPA/src/main/java/views/MainFrame.java
@@ -5,6 +5,7 @@
 package views;
 
 import models.Usuario;
+import java.awt.BorderLayout;
 
 /**
  *
@@ -16,16 +17,123 @@ public class MainFrame extends javax.swing.JFrame {
      * Creates new form MainFrame
      */
     private Usuario usuarioLogueado;
+    private boolean modoAdmin = false;
+    private boolean mostrarSpoilers = true;
+    
+    // Instancias de cada pestaña para comunicación
+    private PublicarTab publicarTab;
+    private CategoriasTab categoriasTab;
+    private JuegosTab juegosTab;
+    private UsuariosTab usuariosTab;
+    private ReseñasTab reseñasTab;
+    private CrudTab crudTab;
+    private AjustesTab ajustesTab;
+    
     public MainFrame() {
         initComponents();
         setLocationRelativeTo(null);
-        LoginDialolog loginDialog = new LoginDialolog(this,true);
+        
+        // Login
+        LoginDialolog loginDialog = new LoginDialolog(this, true);
         loginDialog.setVisible(true);
         usuarioLogueado = loginDialog.getUsuarioLogueado();
         if (usuarioLogueado == null) {
             System.exit(0);
         }
+        PopUpAjustes ajustesDialog = new PopUpAjustes(this, true);
+        ajustesDialog.setVisible(true);
+
+        // Configurar pestañas
+        configurarPestañas();
+        
         this.setVisible(true);
+    }
+    
+    private void configurarPestañas() {
+        // Crear instancias de cada pestaña
+        publicarTab = new PublicarTab(this);
+        categoriasTab = new CategoriasTab(this);
+        juegosTab = new JuegosTab(this);
+        usuariosTab = new UsuariosTab(this);
+        reseñasTab = new ReseñasTab(this);
+        crudTab = new CrudTab(this);
+        ajustesTab = new AjustesTab(this);
+        
+        // Configurar layout de cada JPanel contenedor
+        tabBuscar.setLayout(new BorderLayout());
+        tabCategorias.setLayout(new BorderLayout());
+        tabJuegos.setLayout(new BorderLayout());
+        tabUsuarios.setLayout(new BorderLayout());
+        tabReseñas.setLayout(new BorderLayout());
+        tabCrud.setLayout(new BorderLayout());
+        tabAjustes.setLayout(new BorderLayout());
+        
+        // Añadir contenido a cada JPanel contenedor
+        tabBuscar.add(publicarTab, BorderLayout.CENTER);
+        tabCategorias.add(categoriasTab, BorderLayout.CENTER);
+        tabJuegos.add(juegosTab, BorderLayout.CENTER);
+        tabUsuarios.add(usuariosTab, BorderLayout.CENTER);
+        tabReseñas.add(reseñasTab, BorderLayout.CENTER);
+        tabCrud.add(crudTab, BorderLayout.CENTER);
+        tabAjustes.add(ajustesTab, BorderLayout.CENTER);
+        
+        // Configurar visibilidad inicial de pestaña CRUD
+        actualizarVisibilidadCrud();
+    }
+    
+    // Métodos para navegación entre pestañas
+    public void irAReseñasDeJuego(models.Videojuego juego) {
+        reseñasTab.mostrarReseñasDeJuego(juego);
+        tabPadre.setSelectedComponent(tabReseñas);
+    }
+    
+    public void irAReseñasDeUsuario(models.Usuario usuario) {
+        reseñasTab.mostrarReseñasDeUsuario(usuario);
+        tabPadre.setSelectedComponent(tabReseñas);
+    }
+    
+    public void irAJuegosDeCategoria(models.Categoria categoria) {
+        juegosTab.filtrarPorCategoria(categoria);
+        tabPadre.setSelectedComponent(tabJuegos);
+    }
+    
+    // Métodos para gestión de configuración
+    public void setModoAdmin(boolean modoAdmin) {
+        this.modoAdmin = modoAdmin;
+        actualizarVisibilidadCrud();
+    }
+    
+    public void setMostrarSpoilers(boolean mostrarSpoilers) {
+        this.mostrarSpoilers = mostrarSpoilers;
+    }
+    
+    private void actualizarVisibilidadCrud() {
+        int indiceCrud = tabPadre.indexOfTab("Crud");
+        
+        if (modoAdmin) {
+            // Mostrar pestaña CRUD si no está visible
+            if (indiceCrud == -1) {
+                tabPadre.insertTab("Crud", null, tabCrud, null, tabPadre.getTabCount() - 1);
+            }
+        } else {
+            // Ocultar pestaña CRUD si está visible
+            if (indiceCrud != -1) {
+                tabPadre.removeTabAt(indiceCrud);
+            }
+        }
+    }
+    
+    // Getters para acceso desde pestañas
+    public Usuario getUsuarioLogueado() {
+        return usuarioLogueado;
+    }
+    
+    public boolean isModoAdmin() {
+        return modoAdmin;
+    }
+    
+    public boolean isMostrarSpoilers() {
+        return mostrarSpoilers;
     }
 
     /**
@@ -42,6 +150,8 @@ public class MainFrame extends javax.swing.JFrame {
         tabCategorias = new javax.swing.JPanel();
         tabJuegos = new javax.swing.JPanel();
         tabUsuarios = new javax.swing.JPanel();
+        tabReseñas = new javax.swing.JPanel();
+        tabCrud = new javax.swing.JPanel();
         tabAjustes = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -57,7 +167,7 @@ public class MainFrame extends javax.swing.JFrame {
             .addGap(0, 265, Short.MAX_VALUE)
         );
 
-        tabPadre.addTab("Buscar", tabBuscar);
+        tabPadre.addTab("Publicar", tabBuscar);
 
         javax.swing.GroupLayout tabCategoriasLayout = new javax.swing.GroupLayout(tabCategorias);
         tabCategorias.setLayout(tabCategoriasLayout);
@@ -97,6 +207,32 @@ public class MainFrame extends javax.swing.JFrame {
         );
 
         tabPadre.addTab("Usuarios", tabUsuarios);
+
+        javax.swing.GroupLayout tabReseñasLayout = new javax.swing.GroupLayout(tabReseñas);
+        tabReseñas.setLayout(tabReseñasLayout);
+        tabReseñasLayout.setHorizontalGroup(
+            tabReseñasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 531, Short.MAX_VALUE)
+        );
+        tabReseñasLayout.setVerticalGroup(
+            tabReseñasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 265, Short.MAX_VALUE)
+        );
+
+        tabPadre.addTab("Reseñas", tabReseñas);
+
+        javax.swing.GroupLayout tabCrudLayout = new javax.swing.GroupLayout(tabCrud);
+        tabCrud.setLayout(tabCrudLayout);
+        tabCrudLayout.setHorizontalGroup(
+            tabCrudLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 531, Short.MAX_VALUE)
+        );
+        tabCrudLayout.setVerticalGroup(
+            tabCrudLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 265, Short.MAX_VALUE)
+        );
+
+        tabPadre.addTab("Crud", tabCrud);
 
         javax.swing.GroupLayout tabAjustesLayout = new javax.swing.GroupLayout(tabAjustes);
         tabAjustes.setLayout(tabAjustesLayout);
@@ -167,8 +303,10 @@ public class MainFrame extends javax.swing.JFrame {
     private javax.swing.JPanel tabAjustes;
     private javax.swing.JPanel tabBuscar;
     private javax.swing.JPanel tabCategorias;
+    private javax.swing.JPanel tabCrud;
     private javax.swing.JPanel tabJuegos;
     private javax.swing.JTabbedPane tabPadre;
+    private javax.swing.JPanel tabReseñas;
     private javax.swing.JPanel tabUsuarios;
     // End of variables declaration//GEN-END:variables
 }
